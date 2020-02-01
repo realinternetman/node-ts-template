e415aae490a97fa7ad28965e0a3eda791b73887c
realinternetman <ysrim891@gmail.com>
2020-02-01 23:54:42 +0900

    Update .eslintrc for interfaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,21 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "@typescript-eslint/no-explicit-any": 0,
+    "semi": "off",
+    "@typescript-eslint/semi": ["error", "never"],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      // For interface
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false
+        }
+      }
+    ]
   }
 };
